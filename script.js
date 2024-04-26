cc1689e9c0ef43ff449ca03d97751fac2421efa0
user.name like: "krunal022 <user.name like :"krunal022>
2024-04-26 12:25:23 +0530

    modifications

---
+++ script.js
@@ -0,0 +1,21 @@
+Shery.mouseFollower({
+    skew: true,
+    ease: "cubic-bezier( 0.59, 0.51, 0.18, 0.45 )",
+    duration: .7,
+  });
+
+  gsap.from(".nlink",{
+    stagger:.1,
+    y:10,
+    duration:1,
+    ease:Power2,
+    opacity:0
+});
+
+  Shery.textAnimate("#herotext h1", {
+    style: 1,
+    y: 10,
+    delay: 0.1,
+    duration: .5,
+  });
+  
