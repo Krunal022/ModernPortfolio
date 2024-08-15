aa1c1eec906bade62efa9a7bde3e59f2a92ac17e
Krunal022 <krunalvaghela723@gmail.com>
2024-08-15 13:04:47 +0530

    Enhanced portfolio with responsive layout

--- script.js
+++ script.js
@@ -1,25 +1,25 @@
 Shery.mouseFollower({
-    skew: true,
-    ease: "cubic-bezier( 0.59, 0.51, 0.18, 0.45 )",
-    duration: .7,
-  });
-
-  gsap.from(".nlink",{
-    stagger:.1,
-    y:10,
-    duration:1,
-    ease:Power2,
-    opacity:0
+  skew: true,
+  ease: "cubic-bezier( 0.59, 0.51, 0.18, 0.45 )",
+  duration: .7,
 });
 
-  Shery.textAnimate("#herotext h1", {
-    style: 1,
-    y: 10,
-    delay: 0.1,
-    duration: .5,
-  });
-  
-  Shery.makeMagnet(".nlink" ,{
-    ease: "cubic-bezier(0.23, 1, 0.320, 1)",
-    duration: 1,
-  });
+gsap.from(".nlink",{
+  stagger:.5,
+  y:100,
+  duration:1.2,
+  ease:Power2,
+  opacity:0
+});
+
+Shery.textAnimate("#heroleft h1,.hand", {
+  style: 1,
+  y: 10,
+  delay: 0.1,
+  duration: .5,
+});
+
+Shery.makeMagnet(".nlink" ,{
+  ease: "cubic-bezier(0.23, 1, 0.320, 1)",
+  duration: 1,
+});
